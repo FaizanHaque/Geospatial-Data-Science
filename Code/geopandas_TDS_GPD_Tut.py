2a65c55f87889904630bbf067076142ba74b1872
FaizanHaque <sfhaque@uci.edu>
2019-04-05 23:39:42 -0700

    Initialize
    Initialize

---
+++ Code/geopandas_TDS_GPD_Tut.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 22:37:46 2019
+
+@author: Home
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import descartes
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+
+%matplotlib inline
+street_map = 
+
---
+++ Code/gpd_netanalysis_tut.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 22:55:57 2019
+
+@author: Home
+"""
+
+   # all imports should go here
+
+import pandas as pd
+import sys
+import os
+import subprocess
+import datetime
+import platform
+import datetime
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
+import geopandas as gpd
+import shapely
+from shapely.geometry import Polygon
+
+import numpy as np
+from matplotlib.collections import PatchCollection
+from descartes import PolygonPatch
+
+import math
